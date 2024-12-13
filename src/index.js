a9ac8713493ef4e26e697288d8cb1bfdfa7981fc
Sophiaazaliah <Sophiaazaliah10@gmail.com>
2024-12-13 10:16:04 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function checkGender(){
+} var gender = prompt ("please enter your gender (male or female):");
+if (gender === "female"){cd
+    alert("you are a girl! 👩");
+}else if (gender === "male"){
+    alert("you are a boy! 🧑");
+}else{
+    alert("invalid input. please enter 'male' or 'female'");
+}
+checkGender();
